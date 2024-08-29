6bb9856b0411b5010b9933a8244069c676fda192
Richard Stacey <staceyrichard1996@gmail.com>
2024-08-29 14:57:59 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,48 @@
+let targetMonth = "October";
+let targetDay = 28;
+let targetYear = 2024;
+
+
+const countdownDate = new Date("October 28, 2024 21:00:00").getTime();
+
+const myfunc = setInterval(function() {
+    const now = new Date().getTime();
+    const timeLeft = countdownDate - now;
+    
+    const days  = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    if (days == 1) {
+    document.getElementById('days').innerHTML = days + " Day,";
+    } else {        
+    document.getElementById('days').innerHTML = days + " Days,";
+    }
+
+    if (hours == 1) {
+        document.getElementById('hours').innerHTML = hours + " Hour,";
+    } else {
+        document.getElementById('hours').innerHTML = hours + " Hours,";
+    }
+
+    if (minutes == 1) {
+        document.getElementById('mins').innerHTML = minutes + " Minute,";
+    } else {
+        document.getElementById('mins').innerHTML = minutes + " Minutes,";
+    }
+
+    if (seconds == 1) {
+        document.getElementById('secs').innerHTML = seconds + " Second";
+    } else {
+        document.getElementById('secs').innerHTML = seconds + " Seconds";
+    }
+
+    if (timeLeft <= 0) {
+        document.getElementById('days').innerHTML = "";
+        document.getElementById('hours').innerHTML = "";
+        document.getElementById('mins').innerHTML = "";
+        document.getElementById('secs').innerHTML = "";
+        document.getElementById('end').innerHTML = "12:00 AM, Night 1, let the terror Begin!"
+    }
+}, 1000)
